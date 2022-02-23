2a3fa6f66744746969d3f66a0a2439f4b217cc14
hugompt <hugomsfh@hotmail.com>
2022-02-23 20:11:06 +0100

    02 - done!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+let aux = string.toString();
+    if (num > 0){    
+        for (; num > 1; --num){
+            aux += string;
+        }
+        return aux
+    }else if(num === 0){
+        return ""
+    }else if(num < 0){
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
