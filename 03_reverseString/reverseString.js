8a45a5cebb861616e79bd7ebb5b11b8f07fe1590
hugompt <hugomsfh@hotmail.com>
2022-02-23 20:20:37 +0100

    03 - done!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let aux = "";
+    for (let i = (string.length - 1); i >= 0; i--){
+        aux += string.substr(i,1);
+    }
+    return aux
 };
 
 // Do not edit below this line
