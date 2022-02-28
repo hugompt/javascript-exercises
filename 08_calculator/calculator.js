7c9de31c5925b700ddff01afccca64f79f07fa62
hugompt <hugomsfh@hotmail.com>
2022-02-28 22:18:15 +0100

    CHANGELOG - completed almost all the tests successfully - only need to finish the last one

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,38 @@
-const add = function() {
-	
+//add, subtract, get the sum, multiply, get the power, and find the factorial
+
+const add = function(numb1, numb2) {
+	return numb1+numb2;
 };
 
-const subtract = function() {
-	
+const subtract = function(numb1, numb2) {
+	return numb1-numb2;
 };
 
-const sum = function() {
-	
+const sum = function(array1) {
+  let arraySum = 0;
+	for (i = 0; i < array1.length; i++){
+    arraySum += array1[i];
+  }
+  return arraySum
 };
 
-const multiply = function() {
+const multiply = function(numbersMulti) {
+  let arrayMulti = 1;
+  let i = 0;
 
+  while (i < (numbersMulti.length)){
+    arrayMulti = arrayMulti * numbersMulti[i];
+    i++;
+  }
+  return arrayMulti
 };
 
-const power = function() {
-	
+const power = function(numb1, numb2) {
+	let power = 1
+  for (i = numb2; i > 0 ; i--){
+    power = power * numb1;
+  }
+  return power
 };
 
 const factorial = function() {
