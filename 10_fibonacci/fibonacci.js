2f3193ffc3dfedf4aba13863d278c4ae23fe9357
hugompt <hugomsfh@hotmail.com>
2022-02-28 23:54:15 +0100

    CHANGELOG - Needs revision, just started coding, not working (yet!)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(fiboNum) {
+    let counter = 0;
+    for (i = 1; i <= fiboNum; i++){
+        console.log(counter + " 1");
+        counter += counter + (i-1); 
+        console.log(counter + " 2");
+    }
+    return counter;
 };
 
 // Do not edit below this line
