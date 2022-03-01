4d944dd3706321737ec4dab32637b5dde906b56f
hugompt <hugomsfh@hotmail.com>
2022-03-01 21:23:57 +0100

    CHANGELOG - Solved!

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let booksTitles = books.map(function(element){
+        return `${element.title}`
+    })
+    return booksTitles
 };
 
 // Do not edit below this line
