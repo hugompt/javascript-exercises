464c46dc69e8e4108be621a25ff5ae26b3cebaef
hugompt <hugomsfh@hotmail.com>
2022-03-01 21:14:46 +0100

    CHANGELOG done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,16 +1,16 @@
 const fibonacci = function(fiboNum) {
-    let counter = 0;
-    let n1 = 0;
-    let n2 = 1 ;
-    for (i = 0; i < fiboNum; i++){
-        if (fiboNum === 1){
-            return fiboNum;
-        }else{
-            counter += n1 + n2; 
-            console.log(counter + " " + i);
+    let num1 = 0;
+    let num2 = 1;
+    let fibo = 0;
+
+    if (fiboNum >= 0) {
+        for (i = 1; i < fiboNum; i++){
+            fibo = num1 + num2;
+            num1 = num2;
+            num2 = fibo;
         }
-    }
-    return counter;
+        return num2;
+    }else return "OOPS";    
 };
 
 // Do not edit below this line
